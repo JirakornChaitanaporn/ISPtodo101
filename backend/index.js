10565dc872a497ef85dc59ff8900d4b4d98b8288
JirakornChaitanaporn <jirakorn.c@ku.th>
2025-08-14 16:56:46 +0700

    docker again

--- backend/index.js
+++ backend/index.js
@@ -5,54 +5,50 @@ const cors = require('cors');
 const app = express();
 const PORT = 15000;
 
-const mongoURL = process.env.MONGO_URL|| 'mongodb://localhost:27017/todos'
+const mongoURL = process.env.MONGO_URL || 'mongodb://localhost:27017/todos';
 
 app.use(cors());
 app.use(express.json());
 
-//Mongoose Scheme
+//mongose Schema
 const Task = mongoose.model('Task', new mongoose.Schema({
     text: String,
     completed: Boolean
 }));
 
-//Routes
+//ROUTES
 app.get('/tasks', async (req, res) => {
-  const tasks = await Task.find();
-  res.json(tasks);
+    const tasks = await Task.find();
+    res.json(tasks);
 });
 
 app.post('/tasks', async (req, res) => {
-  const task = await Task.create(req.body);
-  res.json(task);
+    const task = await Task.create(req.body);
+    res.json(task);
 });
 
 app.put('/tasks/:id', async (req, res) => {
-  const task = await Task.findByIdAndUpdate(req.params.id, req.body);
-  res.json(task);
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body);
+    res.json(task);
 });
 
 app.delete('/tasks/:id', async (req, res) => {
-  await Task.findByIdAndDelete(req.params.id);
-  res.sendStatus(204);
+    await Task.findByIdAndDelete(req.params.id);
+    res.sendStatus(204);
 });
 
-//connect mongodb and start server when ready only
-const connectWithRetry = () => {
+const connectWithRetry = async () => {
     console.log('Trying to connect to MongoDB...');
-    mongoose.connect(mongoURL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
+    mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
-        console.log('MongoDB connected');
+        console.log('MongoDB connected')
         app.listen(PORT, () => {
             console.log(`Backend running on port ${PORT}`);
         });
     })
     .catch(err => {
         console.error('MongoDB connection error. Retrying in 5s...', err.message);
-        setTimeout(connectWithRetry, 5000);
+        setTimeout(connectWithRetry, 5000); // Retry after 5 seconds
     });
 };
 
